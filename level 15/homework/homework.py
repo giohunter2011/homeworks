2647f896fdca76859b575b84f3f37504ef59631a
giohunter2011 <giorgirusishvili2011@gmail.com>
2024-08-07 16:16:03 +0400

    second commit

--- level 15/homework/homework.py
+++ level 15/homework/homework.py
@@ -1,4 +1,22 @@
 print(True and False)
 
-print(5>3 and 32 < 5)
+print(9>3 and 32 < 33)
+
+print(465>3 and 32 < 76)
+
+print(554>3 and 32 < 213)
+
+print(34>3 and 32 < 90)
+
+print(7>3 and 32 < 564)
+
+print(8>3 and 32 < 43)
+
+print(21>3 and 32 < 54)
+
+print(798>3 and 32 <655)
+
+print(5>3 and 32 < 54)
+
+
 
